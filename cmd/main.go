eb7fbe0bef15634a4af33774fe85997bbac95191
Kirill-Dr <daright6@gmail.com>
2025-07-25 21:35:54 +0600

    feat: auth module

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"link-cutter/internal/auth"
 	"net/http"
 )
 
 func main() {
 	router := http.NewServeMux()
+	auth.NewAuthHandler(router)
 
 	server := http.Server{
 		Addr:    ":8081",
