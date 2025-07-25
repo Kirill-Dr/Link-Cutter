649776c27411d92a962c65ddd909cb9da7f6ca83
Kirill-Dr <daright6@gmail.com>
2025-07-25 20:51:42 +0600

    feat: config and struct of project

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is running")
+	server.ListenAndServe()
+}
---
+++ configs/config.go
@@ -0,0 +1,28 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DB DBConfig
+}
+
+type DBConfig struct {
+	DSN string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using default config")
+	}
+	return &Config{
+		DB: DBConfig{
+			DSN: os.Getenv("DSN"),
+		},
+	}
+}
