95fb62dc4ce49e7797fcd37bdac67fbbcd747f79
Kirill-Dr <daright6@gmail.com>
2025-07-27 15:25:30 +0600

    feat: link handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"link-cutter/configs"
 	"link-cutter/internal/auth"
+	"link-cutter/internal/link"
 	"link-cutter/pkg/db"
 	"net/http"
 )
@@ -12,9 +13,11 @@ func main() {
 	config := configs.LoadConfig()
 	_ = db.NewDB(config)
 	router := http.NewServeMux()
+
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config: config,
 	})
+	link.NewLinkHandler(router, link.LinkHandlerDeps{})
 
 	server := http.Server{
 		Addr:    ":8081",
